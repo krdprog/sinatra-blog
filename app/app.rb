91811ffd91b047a4a85f24cbe3481be6fbf47200
krdprog <info@krdprog.ru>
2019-02-23 14:27:47 +0300

    Refactoring and convert tabs to spaces

--- app/app.rb
+++ app/app.rb
@@ -8,59 +8,59 @@ require 'sqlite3'
 
 # Method connect with database
 def get_db
-	@db = SQLite3::Database.new 'base.db'
-	@db.results_as_hash = true
-	return @db
+  @db = SQLite3::Database.new 'base.db'
+  @db.results_as_hash = true
+  return @db
 end
 
 def get_posts
-	get_db
-	@posts = @db.execute 'SELECT * FROM Posts'
-	@db.close
+  get_db
+  @posts = @db.execute 'SELECT * FROM Posts'
+  @db.close
 end
 
 # Configure application
 configure do
-	get_db
-	# create table Messages in database
-	@db.execute 'CREATE TABLE IF NOT EXISTS "Posts"
-	  (
-		  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-		  "post_title" TEXT,
-		  "post_body" TEXT
-		)'
-	@db.close
+  get_db
+  # create table Messages in database
+  @db.execute 'CREATE TABLE IF NOT EXISTS "Posts"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "post_title" TEXT,
+      "post_body" TEXT
+    )'
+  @db.close
 end
 
 get '/' do
-	@title = 'Our blog'
+  @title = 'Our blog'
 
-	get_posts
+  get_posts
 
-	erb :index
+  erb :index
 end
 
 get '/new' do
-	@title = 'New post'
-	erb :new
+  @title = 'New post'
+  erb :new
 end
 
 post '/new' do
-	@title = 'New post is posted'
-	@post_title = params[:post_title]
-	@post_body = params[:post_body]
+  @title = 'New post is posted'
+  @post_title = params[:post_title]
+  @post_body = params[:post_body]
 
-	get_db
-	@db.execute 'INSERT INTO Posts (post_title, post_body) VALUES (?, ?)', [@post_title, @post_body]
-	@db.close
+  get_db
+  @db.execute 'INSERT INTO Posts (post_title, post_body) VALUES (?, ?)', [@post_title, @post_body]
+  @db.close
 
-	erb :posted
+  erb :posted
 end
 
 get '/admin' do
-	@title = "Admin Panel"
+  @title = "Admin Panel"
 
-	get_posts
+  get_posts
 
-	erb :admin
+  erb :admin
 end
